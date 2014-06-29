5b33418fa6d7af57e1dd6a808755540d4a26a741
Shawn Vega <svega85@gmail.com>
2014-06-29 02:42:34 -0700

    She's broken :(

--- A3.cpp
+++ A3.cpp
@@ -13,65 +13,125 @@ using namespace std;
 /*
 This program shall simulate
 */
-
-int createProc(int *fildes,int numOfSec){
-pid_t pid = fork();
-    const int BUFSIZE =100;
+int process0(int *fildes){
+	const int BUFSIZE =100;
     char buf[BUFSIZE];
     ssize_t nbytes;
+    close(fildes[1]);          /* Close unused write end */
+    cout << "\x1b[33m about to read \e[m" << endl;
+    ///fcntl(mypipe[0], F_SETFL, O_NONBLOCK)
+    struct stat stats;
+    int r;
+    off_t bytesAvail = -1;
+    for(int i =0; i<2; i++){
+		time_t now = time(0);
+		tm *ltm = localtime(&now);
+		cout << "The time is now: " << ltm->tm_hour << "h:" << ltm->tm_min << "m:" << ltm->tm_sec << "s\n";
+        r = fstat(fildes[0], &stats);
+        if (r < 0) perror("fstat failed");
+        else {
+            ///if (S_ISCHR(stats.st_mode)) cout << "S_ISCHR\n";
+            ///else if (S_ISFIFO(stats.st_mode)) cout << "S_ISFIFO\n";
+            ///else if (S_ISREG(stats.st_mode)) cout << "S_ISREG\n";
+            ///else cout << "unknown stat mode\n";
+            bytesAvail = stats.st_size;
+            cout << "\x1b[36m bytes available= \e[m" << (int)bytesAvail<<endl;
+            sleep(1);
+            if(bytesAvail > 0){
+                nbytes = read(fildes[0], buf, BUFSIZE);
+                cout << "\x1b[35m reading data, \e[m" << buf <<endl;
+                break;
+                }
+            }
+        }
+		cout << "\x1b[35m done reading \e[m" << endl;
+        write(STDOUT_FILENO, "\n", 1);
+        close(fildes[0]);
+        cout << "\x1b[32m child pipe closed \e[m" << endl;
+        _exit(EXIT_SUCCESS);
+}
+
+void process1(){
+	cout << "called proc1\n";
+	int sleepT = 3;
+	for(int x = 0; x < 2; x++){
+		pid_t pid = fork();
+		if (pid == 0) { // child process
+			char *argv = "/opt/local/bin/uptime";
+			char *newargv[] = {NULL};
+			char *newenviron[] = { NULL };
+			execve(argv, newargv, newenviron);
+			//_exit(EXIT_SUCCESS);
+		}
+		else 
+			if(pid>0){
+				cout << "proc1 sleeping \n";
+				sleep(sleepT);
+			}
+			else
+				cout << "errorrr";
+		
+		
+	}
+}
+
+void countDown(int *fildes){
+        /* Parent writes argv[1] to pipe */
+        close(fildes[0]);          /* Close unused read end */
+        //char *test = "blah";
+        cout << "\x1b[32m sending \e[m"<<endl;
+        write(fildes[1], "Hello world\n\0", 13);
+        cout << "\x1b[32m done sending \e[m" << endl;
+        close(fildes[1]);          /* Reader will see EOF */
+        cout << "\x1b[33m parent pipe closed \e[m" << endl;
+}
+
+int createProc(int *fildes,int numOfSec){
+
+pid_t pid = fork();
+    
     int counter = 0;
     
     if (pid == 0) // child process
     {
-        
-        int i = 0;
-        for (; i < 1; ++i)
-        {
-            printf(purple "child process: counter=%d" reset "\n", ++counter);
-            
-            sleep(1);
-        }
-        
+		pid_t pid2 = fork();
+		if (pid2 == 0) // grandchild process
+		{
+			pid_t pid3 = fork();
+			if (pid3 == 0) // great-grandchild process
+			{
+				cout << " calling proc countdown\n";
+				countDown(fildes);
+			}
+			else { // grandchild process
+				if (pid3 > 0) {
+					cout << "waiting for proc 3\n";
+					wait(NULL);
+				}
+			}
+			cout << " calling proc 0\n";
+			process0(fildes);
+			wait(NULL);
+		}
+		else if (pid2 > 0) {
+			cout << " calling proc 1\n";
+			process1();
+		}
 
-        close(fildes[1]);          /* Close unused write end */
-        cout << "\x1b[33m about to read \e[m" << endl;
-        
-        //cout << fstat(fildes[0])<<endl;
-        //dup2(fildes[0],1);
-        //while ( == 0){
-        //fcntl(mypipe[0], F_SETFL, O_NONBLOCK)
-        struct stat stats;
-        int r;
-        off_t bytesAvail = -1;
-        for(int i =0; i<2; i++){
-            r = fstat(fildes[0], &stats);
-            if (r < 0) perror("fstat failed");
-            else {
-                //if (S_ISCHR(stats.st_mode)) cout << "S_ISCHR\n";
-                //else if (S_ISFIFO(stats.st_mode)) cout << "S_ISFIFO\n";
-                //else if (S_ISREG(stats.st_mode)) cout << "S_ISREG\n";
-                //else cout << "unknown stat mode\n";
-                bytesAvail = stats.st_size;
-                cout << "\x1b[36m bytes available= \e[m" << (int)bytesAvail<<endl;
-                sleep(1);
-            }
-        }
-            nbytes = read(fildes[0], buf, BUFSIZE);
+        // int i = 0;
+        // for (; i < 1; ++i)
+        // {
+            // printf(purple "child process: counter=%d" reset "\n", ++counter);
+        // }
+
+            //nbytes = read(fildes[0], buf, BUFSIZE);
             //write(STDOUT_FILENO, buf, sizeof(buf));
-            cout << "\x1b[35m reading data, \e[m" << buf <<endl;
+            //cout << "\x1b[35m reading data, \e[m" << buf <<endl;
             //nbytes = read(fildes[0], buf, BUFSIZE);// == 0);//{
             //write(STDOUT_FILENO, buf, BUFSIZE);
             //cout << "reading data," << (int)nbytes<<endl;
             //}
-        cout << "\x1b[35m done reading \e[m" << endl;
-        write(STDOUT_FILENO, "\n", 1);
-        close(fildes[0]);
-        cout << "\x1b[32m child pipe closed \e[m" << endl;
-        char *argv = "/opt/local/bin/uptime";
-        char *newargv[] = {NULL};
-        char *newenviron[] = { NULL };
-        execve(argv, newargv, newenviron);
-        _exit(EXIT_SUCCESS);
+        
     }
     else if (pid > 0)
     {
@@ -82,19 +142,14 @@ pid_t pid = fork();
             printf(purple "parent process: counter=%d" reset "\n", ++counter);
             sleep(1);
         }
-        /* Parent writes argv[1] to pipe */
-        close(fildes[0]);          /* Close unused read end */
-        //char *test = "blah";
-        cout << "\x1b[32m sending \e[m"<<endl;
-        write(fildes[1], "Hello world\n\0", 13);
-        cout << "\x1b[32m done sending \e[m" << endl;
-        close(fildes[1]);          /* Reader will see EOF */
-        cout << "\x1b[33m parent pipe closed \e[m" << endl;
-        wait(NULL);                /* Wait for child */
-        //int returnStatus;
-        //waitpid(pid, &returnStatus, 0);
+
+        ///wait(NULL);                /* Wait for child */
+		///wait(NULL);					// wait for second child
+        int returnStatus;
+        waitpid(pid, &returnStatus, 0);
         cout << "\x1b[32m closing parent \e[m" <<endl;
-        exit(EXIT_SUCCESS);
+        return 0;
+		//exit(EXIT_SUCCESS);
     }
     else
     {
@@ -104,21 +159,24 @@ pid_t pid = fork();
     }
 }
 
+
+
 int main(int argc, char **argv){
     //const int PAGESIZE = getInput("Please enter the number of pages: ");
     int numOfSec = 10;
     int procNum = 0;
     int fildes[2];
 
-
+	cout << endl << endl << endl ;
     
     if (pipe(fildes) == -1) {
         perror("pipe");
         exit(EXIT_FAILURE);
     }
+	
     int status = createProc(fildes,numOfSec);
     if(status != 0){
-        cout << "Error" << endl;
+        //cout << "Error" << endl;
         return 1;
         }
 
@@ -128,227 +186,227 @@ int main(int argc, char **argv){
 
 
 
-vector<int> readFile(char *filename){
-    ifstream infile(filename);
-    string line;
-    getline(infile, line); //iterates each line in file
-    istringstream strstr(line);
-    vector<int> intVec;
-    string word = "";
-    while (getline(strstr,word, ' ')){ // this while loop helps skip blank spaces.
-        int result = atoi(word.c_str());
-        intVec.push_back(result);
-    }
+// vector<int> readFile(char *filename){
+    // ifstream infile(filename);
+    // string line;
+    // getline(infile, line); //iterates each line in file
+    // istringstream strstr(line);
+    // vector<int> intVec;
+    // string word = "";
+    // while (getline(strstr,word, ' ')){ // this while loop helps skip blank spaces.
+        // int result = atoi(word.c_str());
+        // intVec.push_back(result);
+    // }
 
-    return intVec;
-}
+    // return intVec;
+// }
 
-void opt(const vector<int> &vec, const int PAGESIZE){
-    int faults = 0;
-    int lastItem = 0;
-    int pageMem[PAGESIZE][2];
-    int arrow = 0;
-    const int VECSIZE = vec.size();
-    //const int VECSIZE = 20;
+// void opt(const vector<int> &vec, const int PAGESIZE){
+    // int faults = 0;
+    // int lastItem = 0;
+    // int pageMem[PAGESIZE][2];
+    // int arrow = 0;
+    // const int VECSIZE = vec.size();
+    ////const int VECSIZE = 20;
 
     
-    clkZeros(pageMem, PAGESIZE);
-    for(int x = 0; x < VECSIZE; x++){
-        int loc;
-        decrement(pageMem, PAGESIZE);
-        int nextInst= -1;
-        //cout << "nextInst(-1) =" << nextInst << endl;
+    // clkZeros(pageMem, PAGESIZE);
+    // for(int x = 0; x < VECSIZE; x++){
+        // int loc;
+        // decrement(pageMem, PAGESIZE);
+        // int nextInst= -1;
+        ////cout << "nextInst(-1) =" << nextInst << endl;
 
-        loc = contains(vec[x],pageMem, PAGESIZE);
+        // loc = contains(vec[x],pageMem, PAGESIZE);
         
-        //////////////////////////////////////////////////
-        //debugger(vec[x],pageMem, PAGESIZE);
-        //cout << "looking at " << vec[x] << endl;
-        //////////////////////////////////////////////////
+        ////////////////////////////////////////////////
+        ////debugger(vec[x],pageMem, PAGESIZE);
+        ////cout << "looking at " << vec[x] << endl;
+        ////////////////////////////////////////////////
         
-        if ( loc == -1){
-            faults++;
-            loc = findBest(pageMem, PAGESIZE);
-            pageMem[loc][0] = vec[x];
-        }
-        for(int s = x; s < VECSIZE - 1;){
-            s++;
-            if((nextInst == -1) && (vec[s] == pageMem[loc][0])){
-                nextInst = s-x;
-                }
-        }
-        pageMem[loc][1] = nextInst;
-    }
-    cout << "opt faults = " << faults << endl;
-}
+        // if ( loc == -1){
+            // faults++;
+            // loc = findBest(pageMem, PAGESIZE);
+            // pageMem[loc][0] = vec[x];
+        // }
+        // for(int s = x; s < VECSIZE - 1;){
+            // s++;
+            // if((nextInst == -1) && (vec[s] == pageMem[loc][0])){
+                // nextInst = s-x;
+                // }
+        // }
+        // pageMem[loc][1] = nextInst;
+    // }
+    // cout << "opt faults = " << faults << endl;
+// }
 
-void lru(const vector<int> & vec, const int PAGESIZE){
-    deque<int> pageMem (PAGESIZE,0);
-    int faults = 0; 
-    const int VECSIZE = vec.size();
+// void lru(const vector<int> & vec, const int PAGESIZE){
+    // deque<int> pageMem (PAGESIZE,0);
+    // int faults = 0; 
+    // const int VECSIZE = vec.size();
     
-    int loc;
-    for(int x = 0; x < VECSIZE; x++){
-        if(dequeContains(vec[x], pageMem)){
-            loc = dequeFind(vec[x], pageMem);
-            dequeSwap(pageMem,0,loc);
-        }
-        else{
-            faults++;
-            pageMem.pop_back();
-            pageMem.push_front(vec[x]);
-        }
-    }
-    cout << "lru faults = " << faults << endl;
-}
+    // int loc;
+    // for(int x = 0; x < VECSIZE; x++){
+        // if(dequeContains(vec[x], pageMem)){
+            // loc = dequeFind(vec[x], pageMem);
+            // dequeSwap(pageMem,0,loc);
+        // }
+        // else{
+            // faults++;
+            // pageMem.pop_back();
+            // pageMem.push_front(vec[x]);
+        // }
+    // }
+    // cout << "lru faults = " << faults << endl;
+// }
 
-void clk(const vector<int> &vec, const int PAGESIZE){
-    int faults = 0;
-    int lastItem = 0;
-    int pageMem[PAGESIZE][2];
-    int arrow = 0;
-    const int VECSIZE = vec.size();
+// void clk(const vector<int> &vec, const int PAGESIZE){
+    // int faults = 0;
+    // int lastItem = 0;
+    // int pageMem[PAGESIZE][2];
+    // int arrow = 0;
+    // const int VECSIZE = vec.size();
     
-    clkZeros(pageMem, PAGESIZE);
-    for(int x = 0; x < VECSIZE; x++){
-        int loc;
-        if ( (loc = contains(vec[x],pageMem, PAGESIZE)) != -1){
-            pageMem[loc][1] = 1;
-        }
-        else{
-            while(true){ 
-                if(arrow == PAGESIZE)
-                        arrow = 0;
-                if(pageMem[arrow][0] == vec[x]){
-                    pageMem[arrow][1] = 1;
-                    arrow++;
-                    break;
-                }
-                else{
-                    if(pageMem[arrow][1] == 0){
-                        faults++;
-                        pageMem[arrow][0] = vec[x];
-                        pageMem[arrow][1] = 1;
-                        arrow++;
-                        break;
-                    }
-                    pageMem[arrow][1] = 0;
-                }
-                arrow++;
-            }
-        }
-    }
-    cout << "clk faults = " << faults << endl;
-}
+    // clkZeros(pageMem, PAGESIZE);
+    // for(int x = 0; x < VECSIZE; x++){
+        // int loc;
+        // if ( (loc = contains(vec[x],pageMem, PAGESIZE)) != -1){
+            // pageMem[loc][1] = 1;
+        // }
+        // else{
+            // while(true){ 
+                // if(arrow == PAGESIZE)
+                        // arrow = 0;
+                // if(pageMem[arrow][0] == vec[x]){
+                    // pageMem[arrow][1] = 1;
+                    // arrow++;
+                    // break;
+                // }
+                // else{
+                    // if(pageMem[arrow][1] == 0){
+                        // faults++;
+                        // pageMem[arrow][0] = vec[x];
+                        // pageMem[arrow][1] = 1;
+                        // arrow++;
+                        // break;
+                    // }
+                    // pageMem[arrow][1] = 0;
+                // }
+                // arrow++;
+            // }
+        // }
+    // }
+    // cout << "clk faults = " << faults << endl;
+// }
 
-void fifo(const vector<int> &vec, const int PAGESIZE){
-    int faults = 0;
-    int lastItem = 0;
-    int pageMem[PAGESIZE];
-    const int VECSIZE = vec.size();
+// void fifo(const vector<int> &vec, const int PAGESIZE){
+    // int faults = 0;
+    // int lastItem = 0;
+    // int pageMem[PAGESIZE];
+    // const int VECSIZE = vec.size();
     
-    zeros(pageMem, PAGESIZE);
-    for(int x = 0; x < VECSIZE; x++){
-        if(contains(vec[x], pageMem, PAGESIZE))
-            continue;
-        else{
-            faults++;
-            pageMem[lastItem++] = vec[x];
-            if(lastItem == PAGESIZE)
-                lastItem = 0;
-        }
-    }
-    cout << "fifo faults = " << faults << endl;
-}
+    // zeros(pageMem, PAGESIZE);
+    // for(int x = 0; x < VECSIZE; x++){
+        // if(contains(vec[x], pageMem, PAGESIZE))
+            // continue;
+        // else{
+            // faults++;
+            // pageMem[lastItem++] = vec[x];
+            // if(lastItem == PAGESIZE)
+                // lastItem = 0;
+        // }
+    // }
+    // cout << "fifo faults = " << faults << endl;
+// }
 
-int getInput(string prompt){
-     string input = "";
-    int number = 0;
+// int getInput(string prompt){
+     // string input = "";
+    // int number = 0;
 
-    while (true) {
-        cout << prompt;
-        getline(cin, input);
-        stringstream myStream(input);
-        if (myStream >> number && number < 100 && number > 0)
-            break;
-        cout << "Error: Invalid number" << endl;
-    }
-    return number;
-}
+    // while (true) {
+        // cout << prompt;
+        // getline(cin, input);
+        // stringstream myStream(input);
+        // if (myStream >> number && number < 100 && number > 0)
+            // break;
+        // cout << "Error: Invalid number" << endl;
+    // }
+    // return number;
+// }
 
-/////////////////////////////////////////////////////////////////////
-// HELPER METHODS BELOW
-/////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+////HELPER METHODS BELOW
+///////////////////////////////////////////////////////////////////
 
-int dequeFind(const int VALUE,const deque<int> & deq){
-    for(int x = 0; x < deq.size(); x++)
-        if(deq[x] == VALUE)
-            return x;
-    return -1;
-}
+// int dequeFind(const int VALUE,const deque<int> & deq){
+    // for(int x = 0; x < deq.size(); x++)
+        // if(deq[x] == VALUE)
+            // return x;
+    // return -1;
+// }
 
-void dequeSwap(deque<int> & deq,const int loci,const int locj){
-    int temp = deq[loci];
-    deq[loci] = deq[locj];
-    deq[locj] = temp;
-}
-void debug(const int checking,const deque<int> & deq){
-    cout << "Checking for" << checking << "; ";
-    for(int x = 0; x < deq.size(); x++){
-        cout <<deq[x] << " ";
-    }
-    cout << endl;
-}
+// void dequeSwap(deque<int> & deq,const int loci,const int locj){
+    // int temp = deq[loci];
+    // deq[loci] = deq[locj];
+    // deq[locj] = temp;
+// }
+// void debug(const int checking,const deque<int> & deq){
+    // cout << "Checking for" << checking << "; ";
+    // for(int x = 0; x < deq.size(); x++){
+        // cout <<deq[x] << " ";
+    // }
+    // cout << endl;
+// }
 
-bool dequeContains(const int VALUE,const deque<int> deq){
-    for(int x = 0; x < deq.size(); x++)
-        if(deq[x] == VALUE)
-            return true;
-    return false;
-}
+// bool dequeContains(const int VALUE,const deque<int> deq){
+    // for(int x = 0; x < deq.size(); x++)
+        // if(deq[x] == VALUE)
+            // return true;
+    // return false;
+// }
 
-bool contains(const int VALUE,const int arr[], const int ARRSIZE){
-    for(int x = 0; x < ARRSIZE; x++)
-        if(arr[x] == VALUE)
-            return true;
-    return false;
-}
+// bool contains(const int VALUE,const int arr[], const int ARRSIZE){
+    // for(int x = 0; x < ARRSIZE; x++)
+        // if(arr[x] == VALUE)
+            // return true;
+    // return false;
+// }
 
-void zeros(int *arr, const int SIZE){
-for (int x = 0; x < SIZE; x++)
-    arr[x] = 0;
-}
+// void zeros(int *arr, const int SIZE){
+// for (int x = 0; x < SIZE; x++)
+    // arr[x] = 0;
+// }
 
-void clkZeros(int pageMem[][2], int SIZE){
-    for(int x = 0; x < SIZE; x++){
-    pageMem[x][0] =0;
-    pageMem[x][1] =0;
-    }
-}
+// void clkZeros(int pageMem[][2], int SIZE){
+    // for(int x = 0; x < SIZE; x++){
+    // pageMem[x][0] =0;
+    // pageMem[x][1] =0;
+    // }
+// }
 
-int contains(const int VALUE,const int arr[][2], const int ARRSIZE){
-    for(int x = 0; x < ARRSIZE; x++)
-        if(arr[x][0] == VALUE)
-            return x;
-    return -1;
-}
+// int contains(const int VALUE,const int arr[][2], const int ARRSIZE){
+    // for(int x = 0; x < ARRSIZE; x++)
+        // if(arr[x][0] == VALUE)
+            // return x;
+    // return -1;
+// }
 
-void decrement(int pageMem[][2], int SIZE){
-    for(int x = 0; x < SIZE; x++){
-    pageMem[x][1]--;
-    }
-}
+// void decrement(int pageMem[][2], int SIZE){
+    // for(int x = 0; x < SIZE; x++){
+    // pageMem[x][1]--;
+    // }
+// }
 
-int findBest(int pageMem[][2], int SIZE){
-    int high = 0;
-    for(int x = 0; x < SIZE; x++){
-    if(pageMem[x][1] < 0)
-        return x;
-    } 
-    for(int x = 0; x < SIZE; x++){
-        if(pageMem[x][1] > pageMem[high][1])
-            high =x;
-    }
-    return high;
-}
+// int findBest(int pageMem[][2], int SIZE){
+    // int high = 0;
+    // for(int x = 0; x < SIZE; x++){
+    // if(pageMem[x][1] < 0)
+        // return x;
+    // } 
+    // for(int x = 0; x < SIZE; x++){
+        // if(pageMem[x][1] > pageMem[high][1])
+            // high =x;
+    // }
+    // return high;
+// }
 
