4bcec5007c005f0f1a3441a9f7520f619c2c468d
Brigham <kathleen.brigham@gmail.com>
2014-06-30 22:31:02 -0700

    Colors/Formatified
    purdy

--- Clock.cpp
+++ Clock.cpp
@@ -20,7 +20,7 @@ void Clock::process1(int *fildes){
 	while(true){
 		time_t now = time(0);
 		tm *ltm = localtime(&now);
-		cout << "The time is now: " << ltm->tm_hour << "h:" << ltm->tm_min << "m:" << ltm->tm_sec << "s\n";
+		cout << "\x1b[35m The time is now: \e[m \n" << "  " << ltm->tm_hour << "h:" << ltm->tm_min << "m:" << ltm->tm_sec << "s\n";
         sleep(1);
         r = fstat(fildes[0], &stats);
         if (r < 0) perror("process1: fstat failed");
@@ -39,7 +39,7 @@ void Clock::process1(int *fildes){
 
 
 void Clock::process2(int *fildes){
-	cout << "process2: called proc1\n";
+	cout << "process2: called proc1 \n";
 	int sleepT = 1;
 	close(fildes[1]);          /* Close unused write end */
 	pid_t pid;
@@ -85,7 +85,7 @@ void Clock::process3(int *fildes){
 		minutes = seconds / 60;
 		seconds = seconds - (60 * minutes);
 		while(true){
-			cout << "Time Remaining: "<< minutes << ":" << seconds << endl;
+			cout << "\x1b[35m Time Remaining: \e[m" <<  minutes << ":" << seconds << "\n" <<endl;
 			if(seconds == 0 && minutes > 0){
 				seconds=59;
 				minutes--;
